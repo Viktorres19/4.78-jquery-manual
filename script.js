b85b25e1bac5383e5baa212060b92796cceb487e
Viktor Lukyanchenkov <viktor.lukyanchenkov@cd.p2h.com>
2022-07-03 18:22:13 +0300

    add some examples

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-import 'jquery';
+import $ from 'jquery';
 
+
+$(function() {
+    $('.list-item').hover(function() {
+        $(this).toggleClass('active');
+    });
+
+    $('.list-item:eq(2)').on('click', function() {
+        $('.image:even').fadeToggle();
+    });
+
+    // animation on-click
+    $('.list-item:eq(4)').on('click', function() {
+        $('.image:odd').animate({
+            opacity: 'toggle',
+            height: 'toggle'
+        }, 2000);
+    });
+});
